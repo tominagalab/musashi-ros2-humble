9160e0a3ed8f42777f6b44c0754c90c997280edc
f-tominaga <tommy.tommy.walker@gmail.com>
2024-04-01 15:19:29 +0000

    musashi_cameraにlaunchフォルダを追加

--- hardware/musashi_camera/musashi_camera/node_musashi_camera.py
+++ hardware/musashi_camera/musashi_camera/node_musashi_camera.py
@@ -3,22 +3,22 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 
 import cv2
-import neoapi
+# import neoapi
 
 class Musashi_Camera(Node):
     def __init__(self):
-        super().__init__('camera_node')
+        super().__init__('node_musashi_camera')
         
         self.declare_parameter('interval', 0.1)
 
-
-        interval = self.get_parameter('interval').as_double()     
+        interval = self.get_parameter('interval').get_parameter_value().double_value 
+        
         self.timer = self.create_timer(interval, self.timer_callback)
         
     def timer_callback(self):
         self.get_logger().info('timer callback')
-
-def main():
+    
+def main(args=None):
     rclpy.init(args=args)
     node = Musashi_Camera()
     rclpy.spin(node)

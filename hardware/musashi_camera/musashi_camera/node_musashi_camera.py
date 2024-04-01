5e55e0dbe7d805f08449ce6f803edc6730ffd759
f-tominaga <tommy.tommy.walker@gmail.com>
2024-04-01 14:00:16 +0000

    クラス名変更（Musashi_Camera）&timer_callbackの呼び出し周期（interval）をparameter化

--- hardware/musashi_camera/musashi_camera/node_musashi_camera.py
+++ hardware/musashi_camera/musashi_camera/node_musashi_camera.py
@@ -5,19 +5,24 @@ from sensor_msgs.msg import Image
 import cv2
 import neoapi
 
-class Camera(Node):
+class Musashi_Camera(Node):
     def __init__(self):
         super().__init__('camera_node')
-        self.timer = self.create_timer(0.03, self.timer_callback)
+        
+        self.declare_parameter('interval', 0.1)
+
+
+        interval = self.get_parameter('interval').as_double()     
+        self.timer = self.create_timer(interval, self.timer_callback)
         
     def timer_callback(self):
-        pass
+        self.get_logger().info('timer callback')
 
 def main():
     rclpy.init(args=args)
-    camera = Camera()
-    rclpy.spin(camera)
-    camera.destroy_node()
+    node = Musashi_Camera()
+    rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
 
 

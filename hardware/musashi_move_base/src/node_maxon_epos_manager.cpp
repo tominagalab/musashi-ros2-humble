75a5690f0fe79e02b489a916e8842eef898c0ce9
f-tominaga <tommy.tommy.walker@gmail.com>
2024-04-21 14:06:29 +0000

    maxon motor用の自作メッセージを作成した

--- hardware/musashi_move_base/src/node_maxon_epos_manager.cpp
+++ hardware/musashi_move_base/src/node_maxon_epos_manager.cpp
@@ -1,18 +1,28 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+
 #include "rclcpp/rclcpp.hpp"
 
-class maxon_epos_manager: public rclcpp::Node
-{
-  public:
-    maxon_epos_manager() : Node("maxon_epos_manager") {
-    }
+using namespace std::chrono_literals;
 
-  private:
-}
+class MaxonEposManager : public rclcpp::Node {
+ public:
+  MaxonEposManager() : Node("maxon_epos_manager") {
+    _timer = this->create_wall_timer(
+        250ms, std::bind(&MaxonEposManager::timer_callback, this));
+  }
 
-int main(int argc, char * argv[])
+ private:
+  void timer_callback() { RCLCPP_INFO(this->get_logger(), "callback"); }
+
+  rclcpp::TimerBase::SharedPtr _timer;
+};
+
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<maxon_epos_manager>());
+  rclcpp::spin(std::make_shared<MaxonEposManager>());
   rclcpp::shutdown();
   return 0;
 }

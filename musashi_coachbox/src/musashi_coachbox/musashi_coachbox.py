4c121cdf805d2a14e681014c4d623c2246c05973
f-tominaga <tommy.tommy.walker@gmail.com>
2024-05-15 01:13:56 +0000

    musashi_coachbox.pyのコメントを整理した

--- musashi_coachbox/src/musashi_coachbox/musashi_coachbox.py
+++ musashi_coachbox/src/musashi_coachbox/musashi_coachbox.py
@@ -6,28 +6,32 @@ from python_qt_binding.QtCore import QTimer
 
 from musashi_coachbox.musashi_coachbox_widget import CoachBoxWidget
 
-# クラス名は参照されるので書き間違えないこと
+# クラス名はplugin.xmlから参照するのでスペルミスに注意
 class CoachBoxPlugin(Plugin):
   def __init__(self, context):
     super(CoachBoxPlugin, self).__init__(context)
     
-    # オブジェクト名は間違えても動く？未調査
+    # オブジェクト名を設定
     self.setObjectName('CoachBoxPlugin')
-    
+    # contextをもらっておく（?）気にしないでいい
     self._context = context
     
-    # ここでTsuraiWdigetをセットしてつらくなろう
+    # ここでカスタムWidgetクラスの実態を作成する
     self._widget = CoachBoxWidget()
     
     if context.serial_number() > 1:
         self._widget.setWindowTitle(
             self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+    
+    # contextにカスタムWidgetの追加（多分Pluginだからやっている）
     context.add_widget(self._widget)
     
-    # TsuraiWidgetは一定周期で更新したいのでQTimerを使う
+    # カスタムWidgetを一定周期で更新するためのQTimer設定
     self._timer = QTimer()
+    # QTimerのtimeoutシグナルが発行されたらカスタムWidgetのupdateスロット関数を呼び出す
+    # GUI画面が必要に応じて更新される
     self._timer.timeout.connect(self._widget.update)
-    # 16 msec 周期で更新させる
+    # 16msec周期で更新させる
     self._timer.start(16)
     
   def shutdown_plugin(self):

0402e1db1f44953f87403d694e378ba290419178
f-tominaga <tommy.tommy.walker@gmail.com>
2024-05-26 10:16:17 +0000

    refbox_clientモジュールを実装

---
+++ coachbox/refereebox_client/src/refereebox_client/refbox_client.py
@@ -0,0 +1,74 @@
+
+import sys
+import socket
+import threading
+import time
+import json
+
+
+TEAM_IP = '172.16.32.44' # チームに割り振られた固有IP（コーチボックスに設定するアドレスではありません）
+MAX_RECV_SIZE = 1024*4 # byte
+
+class RefBoxClient(threading.Thread):
+  # コンストラクタ
+  def __init__(self,):
+    super(RefBoxClient, self).__init__()
+    
+    # ソケットの作成．ソケットはメンバ変数
+    # IPv4, TCP設定
+    self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    self._isRun = True
+  
+  # デストラクタ
+  def __del__(self,):
+    pass
+  
+  # 接続（サーバへのログイン）処理メソッド
+  def connect(self, address, port):
+    # 接続処理
+    try:
+      print('Try to connect to RefereeBox')
+      self._socket.settimeout(3) # timeout [sec]
+      self._socket.connect((address, port))
+    except Exception as e:
+      print(e)
+      return False
+    
+    # 無事にサーバ（RefereeBox）へ接続ができれば，RefereeBox側で反応がある
+        
+    return True # 接続完了を表す
+    
+  # 切断処理メソッド
+  def disconnect(self,):
+    print('Disconnect from RefereeBox')
+    self._isRun = False
+    self._socket.close()
+    self.join()
+    
+  # 主となる処理（スレッド処理実態）
+  def run(self,):
+    # ブロッキングモード設定
+    self._socket.settimeout(None)
+
+    while self._isRun:
+      # RefereeBoxからのコマンド受信待ち
+      recv = self._socket.recv(MAX_RECV_SIZE) 
+      
+      # コマンドは全てjson形式のテキストデータで送られてきます
+      # データ末尾にはNULL（'\0'）が入れられているみたいです
+      # 文字列（str）型の末尾１文字を削除している
+      recv_json = json.loads(recv.decode('utf-8')[:-1])
+      print('Recieve from RefereeBox: ')
+      print(recv_json)
+      # recv_jsonは辞書型
+      # キーは'command'と'targetTeam'の二つ
+      # 各値を取得する
+      command = recv_json['command']
+      targetTeam = recv_json['targetTeam']
+      
+      # commandの内容（文字列）に従って各プレイヤーへコマンドを送信する
+      if command == COMM_WELCOME:
+        print('Successeed connect to RefereeBox!')
+      else:
+        pass
--- coachbox/refereebox_client/src/refereebox_client/refereebox_client_plugin.py
+++ coachbox/refereebox_client/src/refereebox_client/refereebox_client_plugin.py
@@ -1,7 +1,8 @@
 from qt_gui.plugin import Plugin
-from python_qt_binding.QtCore import QTimer
+from python_qt_binding.QtCore import QTimer, Slot
 
 from refereebox_client.refereebox_client_widget import RefereeBoxClientWidget
+from refereebox_client.refbox_client import RefBoxClient
 
 class RefereeBoxClientPlugin(Plugin):
   def __init__(self, context):
@@ -19,8 +20,38 @@ class RefereeBoxClientPlugin(Plugin):
     self._timer = QTimer()
     self._timer.timeout.connect(self._widget.update)
     self._timer.start(16)
+    
+    # GUIシグナルスロット接続
+    self._widget.chckConnect.stateChanged.connect(lambda: self.onStateChangedChckConnect(self._widget.chckConnect.checkState()))
   
   def shutdown_plugin(self):
     # 終了時はタイマーを止める
     self._timer.stop()
-    pass    
+  
+  def save_settings(self, plugin_settings, instance_settings):
+    pass
+  
+  def restore_settings(self, plugin_settings, instance_settings):
+    pass
+  
+  @Slot()
+  def onStateChangedChckConnect(self, state):
+    if state: # チェックが入った→接続処理
+      # RefBoxClientの新規作成
+      self._refbox_client = RefBoxClient()
+      # IPアドレスとポートを取得
+      refbox_address = self._widget.lnedtIP.text()
+      refbox_port = int(self._widget.lnedtPort.text())
+      # 接続のトライ
+      isConnect = self._refbox_client.connect(refbox_address, refbox_port)
+      
+      if not isConnect: # 失敗
+        print('Connection error, please chech network condition')
+        self._widget.chckConnect.setCheckState(False)
+      else: # 成功
+        self._refbox_client.start()
+    else: # チェックが外れた→切断処理
+      # self._refbox_client.disconnect()
+      # self._refbox_client.join()
+      del self._refbox_client # デストラクタの呼び出し
+      # pythonでは一応自動的にメモリ解放されるっぽい

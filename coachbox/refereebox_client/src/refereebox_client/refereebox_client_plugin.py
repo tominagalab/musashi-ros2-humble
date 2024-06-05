33871c79b8c01d9dd66f0e971f82d556a4eaeca2
f-tominaga <tommy.tommy.walker@gmail.com>
2024-06-05 14:28:17 +0000

    レフェリーボックスに接続失敗した際のエラーメッセージダイアログを追加

--- coachbox/refereebox_client/src/refereebox_client/refereebox_client_plugin.py
+++ coachbox/refereebox_client/src/refereebox_client/refereebox_client_plugin.py
@@ -1,5 +1,6 @@
 from qt_gui.plugin import Plugin
 from python_qt_binding.QtCore import QTimer, Slot
+from python_qt_binding.QtWidgets import QErrorMessage
 
 from refereebox_client.refereebox_client_widget import RefereeBoxClientWidget
 from refereebox_client.refbox_client import RefBoxClient
@@ -8,44 +9,62 @@ from musashi_msgs.msg import RefereeCmd
 
 class RefereeBoxClientPlugin(Plugin):
   def __init__(self, context):
+    # 親クラス(Pluginクラス)のコンストラクタ呼び出し
     super(RefereeBoxClientPlugin, self).__init__(context)
     
+    # 自分の名前をセット
     self.setObjectName('RefereeBoxClientPlugin')
+    # コンテキストとノードのインスタンスを取得
     self._context = context
     self._node = context.node
     
+    # ウィジェットインスタンスを作成
     self._widget = RefereeBoxClientWidget()
+    
+    # 複数立ち上げた時の対策処理でウィンドウ名を変更している
     if context.serial_number() > 1:
       self._widget.setWindowTitle(
         self._widget.windowTitle() + (' (%d)' % context.serial_number()))
     
+    # コンテキストのウィジェットを追加
+    # これがないと画面が表示されない
     context.add_widget(self._widget)  
-
+    
+    # GUI画面更新用のタイマ割り込み
+    self._timer = QTimer()
+    self._timer.timeout.connect(self._widget.update) # timeoutシグナルをupdateスロットに接続
+    self._timer.start(16) # 16msec周期で画面更新
+    
     # GUIシグナルスロット接続
+    # connectボタンの状態変化時のシグナルスロット接続
     self._widget.chckConnect.stateChanged.connect(self.onStateChangedChckConnect)
     
     # パブリッシャー作成
     self._pub_refcmd = self._node.create_publisher(RefereeCmd, '/referee_cmd', 5)
-    
-    self._timer = QTimer()
-    self._timer.timeout.connect(self._widget.update)
-    self._timer.start(16)
   
+  # シャットダウン時処理
   def shutdown_plugin(self):
     # 終了時はタイマーを止める
     self._timer.stop()
   
+  # プラグインの設定保存処理
   def save_settings(self, plugin_settings, instance_settings):
     pass
   
+  # プラグインの設定保存処理
   def restore_settings(self, plugin_settings, instance_settings):
     pass
-  
+
+  #
+  # 以下，スロット関数
+  #
+  # 接続チェックボックスの状態が変化した時に呼び出されるスロット  
   def onStateChangedChckConnect(self, state):
-    if state: # チェックが入った→接続処理
+    if state: # チェックが入った → 接続処理
+      
       # RefBoxClientの新規作成
       self._refbox_client = RefBoxClient()
-      # IPアドレスとポートを取得
+      # IPアドレスとポートをGUIから取得
       refboxIP = self._widget.lnedtIP.text()
       refboxPort = int(self._widget.lnedtPort.text())
       
@@ -53,31 +72,43 @@ class RefereeBoxClientPlugin(Plugin):
       self._node.get_logger().info('Try to connect to RefereeBox [{}:{}] ...'.format(refboxIP, refboxPort))
       isConnect = self._refbox_client.connect(refboxIP, refboxPort)
       
-      if isConnect:
+      if isConnect: # 接続成功時の処理
         self._node.get_logger().info('Successfully connected to RefereeBox')
-        self._refbox_client.recievedCommand.connect(self.onRecievedCommand) # 受信時のシグナルスロット接続
-        self._refbox_client.start() # RefereeBox client スレッドのスタート
-      else:
+        # レフェリーからコマンド受信時のシグナルスロット接続
+        self._refbox_client.recievedCommand.connect(self.onRecievedCommand) 
+        # RefereeBox client スレッドのスタート
+        self._refbox_client.start() 
+
+      else: # 接続失敗時の処理
         self._node.get_logger().error('Failed to connect to RefereeBox')
         self._node.get_logger().error('Please check network connection status')
+        
+        # エラーメッセージダイアログの表示
+        dlg = QErrorMessage(self._widget)
+        dlg.showMessage('Failed to connect to RefereeBox. Please check network connection status.')
+        
         self._widget.chckConnect.setCheckState(False)
         self._refbox_client = None
-    else: # チェックが外れた→切断処理
+        
+    else: # チェックが外れた → 切断処理
       # self._refbox_client.disconnect()
       # self._refbox_client.join()
       self._refbox_client = None # デストラクタの呼び出し
       # pythonでは一応自動的にメモリ解放されるっぽい
       
-  def onRecievedCommand(self, command, targetTeam):
+  # refereebox_clientがrefereeboxから受信した際に呼び出されるスロット関数
+  def onRecievedCommand(self, recv, command, targetTeam):
     self._node.get_logger().info('Recieved from RefereeBox')
     self._node.get_logger().info('command = {}, targetTeam = {}'.format(command, targetTeam))
     
-    # メッセージ作成
-    refereeCmd = RefereeCmd()
+    # GUIに受信した生テキストを表示
+    self._widget.txtRecv.setText(recv)
+    
+    # メッセージの作成
+    refereeCmd = RefereeCmd() # musashi_msgsパッケージで作成済み
     # 値の代入
     refereeCmd.command = command
     refereeCmd.target_team = targetTeam
-    
-    # パブリッシュ
+    # パブリッシュ．player_serverのノードに伝達することが主目的
     self._pub_refcmd.publish(refereeCmd)
     

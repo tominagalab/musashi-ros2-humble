db61c0a5fff8540376bef6ff0e3b93cf35812e1c
f-tominaga <tommy.tommy.walker@gmail.com>
2024-06-09 14:22:23 +0000

    player_server_widget.uiの削除

--- basestation/musashi_rqt_player_server/setup.py
+++ basestation/musashi_rqt_player_server/setup.py
@@ -10,7 +10,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name + '/resource', ['resource/player_server_widget.ui']),
+        ('share/' + package_name + '/resource', ['resource/player_server.ui']),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['plugin.xml']),
         ('lib/' + package_name, ['scripts/player_server'])
--- basestation/musashi_rqt_player_server/src/musashi_rqt_player_server/rqt_player_server.py
+++ basestation/musashi_rqt_player_server/src/musashi_rqt_player_server/rqt_player_server.py
@@ -1,10 +1,16 @@
+import os
+from ament_index_python.resources import get_resource
+from python_qt_binding import loadUi
+from python_qt_binding.QtWidgets import QWidget
 from qt_gui.plugin import Plugin
 from python_qt_binding.QtCore import QTimer, Slot
 
-from musashi_rqt_player_server.player_server_widget import PlayerServerWidget
 from musashi_rqt_player_server.player_server import PlayerServer
 
 from musashi_msgs.msg import RefereeCmd
+
+PKG_NAME = 'musashi_rqt_player_server'
+UI_FILE_NAME = 'player_server.ui'
 
 class RqtPlayerServer(Plugin):
   def __init__(self, context):
@@ -14,15 +20,10 @@ class RqtPlayerServer(Plugin):
     self._context = context
     self._node = context.node
     
-    # ウィジェット実態の作成
-    self._widget = PlayerServerWidget()
-    # 複数出してしまった時に個別のインスタンスが作成されるように一工夫
-    if context.serial_number() > 1:
-      self._widget.setWindowTitle(
-        self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+    # ウィジェットインスタンスを作成
+    # メンバ変数_widgetに.uiファイルが書き込まれる
+    self.create_ui()
     
-    context.add_widget(self._widget)
-          
     # サブスクライバー作成
     self._sub_refcmd = self._node.create_subscription(
       RefereeCmd,
@@ -35,13 +36,41 @@ class RqtPlayerServer(Plugin):
     self._player_server = PlayerServer()
     self._player_server.recievedPlayerData.connect(self.onRecievedPlayerData)
       
-      
-    self._timer = QTimer()
-    self._timer.timeout.connect(self._widget.update)
-    self._timer.start(16)
-    
     self._player_server.open()  # プレイヤーサーバのオープン
     self._player_server.start() # UDP通信の受信スレッド開始
+    
+    # コンテキストに作成したウィジェットを追加
+    # これをしないとGUI画面が表示されない
+    self._context.add_widget(self._widget)  
+    
+    # GUIスレッドのスタート
+    self.start_ui_thread()
+  
+  def create_ui(self):
+    # Qwidget型のメンバ変数作成
+    self._widget = QWidget()
+    # パッケージ名からパッケージのディレクトリパスを取得
+    _, package_path = get_resource('packages', PKG_NAME)
+    # .uiファイルへのパスを作成，取得
+    ui_file = os.path.join(package_path, 'share', PKG_NAME, 'resource', UI_FILE_NAME)
+    # .uiファイルをQWidget型メンバ変数にロード
+    loadUi(ui_file, self._widget)
+    
+    # 複数立ち上げた時の対策処理でウィンドウ名を変更している
+    if self._context.serial_number() > 1:
+      self._widget.setWindowTitle(
+        self._widget.windowTitle() + (' (%d)' % self._context.serial_number()))
+  
+  def start_ui_thread(self):
+    # QTimerのtimeoutシグナルが発行されるたびにQWidgetのupdateスロットが実行される
+    # これをしないと各ウィジェットのシグナルが発行された時に認識されない
+
+    # GUIイベント更新のためのタイマ割り込み
+    self._timer = QTimer()
+    # timeoutシグナルをupdateスロットに接続
+    self._timer.timeout.connect(self._widget.update) 
+    # 16msec周期（33Hz）で画面更新
+    self._timer.start(16) 
     
   def shutdown_plugin(self):
     # 終了時はタイマーを止める
